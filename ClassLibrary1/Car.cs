dfdfd2a71bf63361efcafb15ee9a91d1e58a3b4a
HalfManHalfWolf <156145401+HalfManHalfWolf@users.noreply.github.com>
2024-01-16 20:21:29 -0500

    first version
    starting up objects and menu.

---
+++ ClassLibrary1/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   public class Car
+    {
+        // attributes of the car
+        public string Make {  get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public int Mileage { get; set; }  
+        public string VIN { get; set; }
+
+        // costructor
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+            Mileage = 0;
+        }
+        // methods 
+        public void Drive(int miles)
+        {
+            if(miles > 0)
+            {
+                Mileage += miles;
+            }
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Make: {Make}, Model: {Model}, Year: {Year}, Mileage: {Mileage}");
+        }
+
+
+        public int getMileage()
+        { return Mileage; }
+    }
+}
